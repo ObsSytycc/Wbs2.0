3665c96818914d1dd0e20ac89bf51d775a240651
IAmDevCT <timothy.ofili@charisol.io>
2022-09-12 10:52:05 +0100

    Adds Project data

--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,8 @@ import { navLinks } from './nav-links';
 import { teamList } from './team-list';
 import { projectList } from './project-list';
 import { footerSubNav, socialLink } from './footerData';
+import { blogPost, categoryList } from './blogPost';
+import { reviewList } from './reviewsList';
+import { aboutData } from './aboutData';
 
-export { navLinks, teamList, projectList, footerSubNav, socialLink };
+export { navLinks, teamList, projectList, footerSubNav, socialLink, blogPost, categoryList, reviewList, aboutData };
